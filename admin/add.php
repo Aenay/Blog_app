a14033eb1a6269be05d85bd3c596b470a9c351cc
Aenay <naywintun415@gmail.com>
2025-02-21 09:55:06 +0700

    add new features

---
+++ admin/add.php
@@ -0,0 +1,77 @@
+<?php 
+
+require '../config/config.php';
+
+session_start();
+if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])){
+    header('Location: login.php');
+}
+
+if($_POST){
+  $file= 'images/'.($_FILES['image']['name']);
+  $imageType=pathinfo($file,PATHINFO_EXTENSION);
+
+  if($imageType != 'png' && $imageType != 'jpg' && $imageType != 'jpeg'){
+    echo "<script>alert('Image must be png,jpg,jpeg')</script>";
+  }else{
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $image = $_FILES['image']['name'];
+    move_uploaded_file($_FILES['image']['tmp_name'],$file);
+
+    $stmt = $pdo->prepare("INSERT INTO posts(title,content,image,author_id)VALUES(:title,:content,:image,:author_id)");
+    $result = $stmt->execute(
+      array(':title'=>$title,':content'=>$content,':image'=>$image,':author_id'=>$_SESSION['user_id'])
+    );
+    if($result){
+      echo "<script>alert('Successfully added');window.location.href='index.php'</script>";
+    }
+
+  }
+}
+
+?>
+
+
+<?php  include('header.html')?>
+
+
+            <!-- Main content -->
+            <div class="content">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="card">
+                        <div class="card-body">
+                        <form class="" action="add.php" method="post" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="">Title</label>
+                                <input type="text" class="form-control" name="title" value="" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Content</label><br>
+                                <textarea class="form-control" name="content" rows="8" cols="88"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Image</label><br>
+                                <input type="file" name="image" value="" required>
+                            </div>
+                            <div class="form-group">
+                              <input type="submit" class="btn btn-success" name="" value="Submit">
+                              <a href="index.php" class="btn btn-warning">Back</a>
+                            </div>
+
+                        </form>
+                        </div>
+                        <!-- /.card-body -->
+                      </div>
+                    </div>
+                    <!-- /.card -->
+                  </div>
+
+                  </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /.content -->
+        </div>
+<?php include('footer.html')?>
--- admin/index.php
+++ admin/index.php
@@ -1,224 +1,117 @@
 <?php 
 
+require '../config/config.php';
+
 session_start();
 if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])){
     header('Location: login.php');
 }
 
-
 ?>
 
+<?php include ('header.html') ?>
 
-
-<!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
-</head>
-
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index3.html" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Contact</a>
-                </li>
-            </ul>
-
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Navbar Search -->
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                        <i class="fas fa-search"></i>
-                    </a>
-                    <div class="navbar-search-block">
-                        <form class="form-inline">
-                            <div class="input-group input-group-sm">
-                                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                                <div class="input-group-append">
-                                    <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
-
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
-
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
-                    </div>
-                </div>
-
-                <!-- SidebarSearch Form -->
-                <div class="form-inline">
-                    <div class="input-group" data-widget="sidebar-search">
-                        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
-                                <p>
-                                    Blogs
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
-            </div>
-            <!-- /.sidebar -->
-        </aside>
-
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-12">
-                            <h1 class="m-0">Starter Page</h1>
-                        </div>
-                        <!-- /.col -->
-                        <!-- /.col -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-                <!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
-            <!-- Main content -->
+            <!-- Main Content -->
             <div class="content">
                 <div class="container-fluid">
-                  <div class="row">
-                    <div class="col-md-12">
-                      <div class="card">
-                        <div class="card-header">
-                          <h3 class="card-title">Bordered Table</h3>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <a href="add.php" class="btn btn-success">New Blog Post</a>
+                                </div>
+                                <div class="card-body">
+                                    <?php 
+                                    if(!empty($_GET['pageno'])){
+                                        $pageno = $_GET['pageno'];
+                                    }else{
+                                        $pageno= 1;
+                                    }
+                                    $numOfrecs = 1;
+                                    $offset = ($pageno - 1 )* $numOfrecs;
+
+                                    if(empty($_POST['search'])){
+                                        $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC");
+                                        $stmt->execute();
+                                        $rawResult = $stmt->fetchAll();
+                                        $total_pages = ceil(count($rawResult) / $numOfrecs); 
+    
+    
+                                        $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC LIMIT $offset,$numOfrecs");
+                                        $stmt->execute();
+                                        $result = $stmt->fetchAll();
+                                    }else{
+                                        $searchKey = $_POST['search'];
+                                        $stmt = $pdo->prepare("SELECT * FROM posts WHERE title LIKE '%$searchKey%' ORDER BY id DESC");
+                                        $stmt->execute();
+                                        $rawResult = $stmt->fetchAll();
+                                        $total_pages = ceil(count($rawResult) / $numOfrecs); 
+    
+    
+                                        $stmt = $pdo->prepare("SELECT * FROM posts WHERE title LIKE '%$searchKey%' ORDER BY id DESC LIMIT $offset,$numOfrecs");
+                                        $stmt->execute();
+                                        $result = $stmt->fetchAll();
+                                    }
+                                    ?>
+
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 10px">#</th>
+                                                <th>Title</th>
+                                                <th>Content</th>
+                                                <th style="width: 40px">Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php 
+                                            if ($result) {
+                                                $i = 1;
+                                                foreach ($result as $value) {
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $i; ?></td>
+                                                    <td><?php echo $value['title']; ?></td>
+                                                    <td><?php echo substr($value['content'], 0, 50); ?></td>
+                                                    <td>
+                                                        <div class="btn-group">
+                                                            <div class="container">
+                                                                <a href="edit.php?id=<?php echo $value['id']; ?>" class="btn btn-warning">Edit</a>
+                                                            </div>
+                                                            <div class="container">
+                                                              <a href="delete.php?id=<?php echo $value['id']; ?>" onclick="return confirm('Are you sure you want to delete this content')" class="btn btn-danger">Delete</a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            <?php 
+                                                $i++;
+                                                }
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                    <nav aria-label="Page navigation example" style="float: right;">
+                                        <ul class="pagination">
+                                            <li class="page-item"><a  class="page-link" href="?pageno=1">First</a></li>
+                                            <li class="page-item <?php if( $pageno <= 1){ echo 'disabled';}?>">
+                                                <a  class="page-link" href="<?php if($pageno <= 1){ echo '#';}else{ echo "?pageno=".($pageno-1);}?>">Previous</a></li>
+                                            <li class="page-item"><a  class="page-link" href="#"><?php echo $pageno;?></a></li>
+                                            <li class="page-item <?php if( $pageno >= $total_pages ){ echo 'disabled';}?>">
+                                                <a  class="page-link" href="<?php if($pageno >= $total_pages){ echo '#';}else{ echo "?pageno=".($pageno+1);}?>">Next</a></li>
+                                            <li class="page-item"><a  class="page-link" href="?pageno = <?php echo $total_pages?>">Last</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
                         </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                          <div>
-                            <a href="" type="button" class="btn btn-success">New Blog Post</a>
-                          </div><br>
-                          <table class="table table-bordered">
-                            <thead>
-                              <tr>
-                                <th style="width: 10px">#</th>
-                                <th>Title</th>
-                                <th>Content</th>
-                                <th style="width: 40px">Actions</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td>1.</td>
-                                <td>Sample Title</td>
-                                <td>Sample Content</td>
-                                <td>
-                                  <div class="btn-group">
-                                    <div class="container">
-                                        <a href="#" type="button" class="btn btn-warning">Edit</a>
-                                    </div>
-                                    <div class="container">
-                                        <a href="#" type="button" class="btn btn-danger">Delete</a>
-                                    </div>
-                                    
-                                  </div>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </div>
-                        <!-- /.card-body -->
-                      </div>
                     </div>
-                    <!-- /.card -->
-                  </div>
-
-                  </div>
-                <!-- /.container-fluid -->
+                </div>
             </div>
-            <!-- /.content -->
         </div>
-        <!-- Main Footer -->
-        <footer class="main-footer">
-            <!-- To the right -->
-            <div class="float-right d-none d-sm-inline">
-                <a href="logout.php" class="btn btn-danger">Logout</a>
-            </div>
-            <!-- Default to the left -->
-            <strong>Copyright &copy; 2025 <a href="#">Jackae.co</a>.</strong> All rights reserved.
-        </footer>
+        <!-- /.content-wrapper -->
+
+        <?php include('footer.html'); ?>
     </div>
-    <!-- ./wrapper -->
-
-    <!-- REQUIRED SCRIPTS -->
-
-    <!-- jQuery -->
-    <script src="../plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../dist/js/adminlte.min.js"></script>
 </body>
-
 </html>
