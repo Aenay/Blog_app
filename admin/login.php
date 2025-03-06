52ff27f226ea284abc06b27951df7ab015fc0add
Aenay <naywintun415@gmail.com>
2025-03-07 02:33:40 +0700

    password hash

--- admin/login.php
+++ admin/login.php
@@ -11,8 +11,8 @@ if ($_POST) {
     $stmt -> execute();
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($user) {
-        if($user['password'] == $password){
-            if($user['role' == 1]){
+        if(password_verify($password,$user['password'])){
+            if($user['role'] == 1){
               $_SESSION['user_id'] = $user['id'];
               $_SESSION['role'] = $user['role']; 
               $_SESSION['username'] = $user['name'];
--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if ($_POST) {
     $stmt -> execute();
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($user) {
-        if($user['password'] == $password){
+        if(password_verify($password,$user['password'])){
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['name'];
             $_SESSION['role'] = $user['role']; 
